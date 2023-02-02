d0fb4f8eeec1c8c57d75d5d2d8e8f65461952a40
Ihsan Fauzan <ihsanfauzan76@gmail.com>
2023-02-02 21:56:09 +0700

    inventory service done

---
+++ inventory-service/src/main/java/com/oop/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package com.oop.inventoryservice.repository;
+
+import com.oop.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    Optional<Inventory> findBySkuCode(String skuCode);
+}
