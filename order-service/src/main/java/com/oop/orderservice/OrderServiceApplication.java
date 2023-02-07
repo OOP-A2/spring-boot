1d6e8374707842a5db6d9240c8f60f5948a5b90f
Ihsan Fauzan <ihsanfauzan76@gmail.com>
2023-02-07 16:09:33 +0700

    fix product service

--- order-service/src/main/java/com/oop/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/oop/orderservice/OrderServiceApplication.java
@@ -2,8 +2,14 @@ package com.oop.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EntityScan("com.oop.orderservice.model")
+@EnableEurekaClient
 public class OrderServiceApplication {
 
 	public static void main(String[] args) {
--- order-service/src/main/java/com/oop/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/oop/orderservice/service/OrderService.java
@@ -22,7 +22,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -38,8 +38,8 @@ public class OrderService {
                 .map(OrderLineItems::getSkuCode)
                 .toList();
 
-        InventoryResponse[] inventoryResponsArray = webClient.get()
-                .uri("http://localhost:8082/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
+        InventoryResponse[] inventoryResponsArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory", uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
                 .block();
@@ -60,6 +60,6 @@ public class OrderService {
         orderLineItems.setPrice(orderLineItemsDto.getPrice());
         orderLineItems.setQuantity(orderLineItemsDto.getQuantity());
         orderLineItems.setSkuCode(orderLineItemsDto.getSkuCode());
-        return  orderLineItems;
+        return orderLineItems;
     }
 }
